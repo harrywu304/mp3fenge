d896f35b0a48f9b3febfc6db5b62e36cc2609a52
harrywu304@gmail.com <harrywu304@gmail.com@71313a22-23b6-de8c-0b1d-698e7327ff81>
2011-11-14 15:50:34 +0000

    comment main method

--- trunk/mp3fenge/src/com/google/code/mp3fenge/Mp3Fenge.java
+++ trunk/mp3fenge/src/com/google/code/mp3fenge/Mp3Fenge.java
@@ -180,17 +180,17 @@ public class Mp3Fenge {
 		FileUtil.generateFile(newMp3, mp3datas);
 	}
 	
-	public static void main(String[] args){
-		Mp3Fenge helper = new Mp3Fenge(new File("testdata/eyes_on_me.mp3"));
-		
-		//directly cut out mp3 by time interval
-		helper.generateNewMp3ByTime(new File("testdata/e1.mp3"), 307000, 315000);
-		
-		//get mp3 data by time interval
-		byte[] e2 = helper.getDataByTime(70000, 76000);
-		List<byte[]> mp3datas = new ArrayList<byte[]>();
-		mp3datas.add(e2);
-		FileUtil.generateFile(new File("testdata/e2.mp3"), mp3datas);
-	}
+//	public static void main(String[] args){
+//		Mp3Fenge helper = new Mp3Fenge(new File("testdata/eyes_on_me.mp3"));
+//		
+//		//directly cut out mp3 by time interval
+//		helper.generateNewMp3ByTime(new File("testdata/e1.mp3"), 307000, 315000);
+//		
+//		//get mp3 data by time interval
+//		byte[] e2 = helper.getDataByTime(70000, 76000);
+//		List<byte[]> mp3datas = new ArrayList<byte[]>();
+//		mp3datas.add(e2);
+//		FileUtil.generateFile(new File("testdata/e2.mp3"), mp3datas);
+//	}
 
 }
